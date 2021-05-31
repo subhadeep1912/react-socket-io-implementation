934e286f58d31ac56f5b126a82e2d55e6d272904
subhadeep <subhadeepc19@gmail.com>
2021-05-31 12:42:21 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const io = require('socket.io')(5000, {
+    cors : {
+        origin : "http://localhost:3000"
+    }
+})
+
+io.on( "connection", (socket) => {
+    console.log(socket.id)
+    socket.on("do-something", something => {
+        console.log(something)
+    }) 
+})
